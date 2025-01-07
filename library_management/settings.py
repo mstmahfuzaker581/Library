f25ed8acd7c6c856d046f56dc36b18e496585ba4
Mst. Mahfuza Akter <150541402+mstmahfuzaker581@users.noreply.github.com>
2025-01-07 14:51:18 +0600

    Update settings.py

--- library_management/settings.py
+++ library_management/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER ='mstmahfuzaker581@gmail.com'
+EMAIL_HOST_PASSWORD ='cebluwlnjbiyckzs'
